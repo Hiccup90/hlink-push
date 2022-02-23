d852ff666ffb6a66c34a054c5964660f95339b87
Hiccup90 <3599937@qq.com>
2022-02-23 17:50:13 +0800

    Update hlink-push.sh

--- hlink-push.sh
+++ hlink-push.sh
@@ -12,19 +12,13 @@ export CORP_SECRET="密匙"
 export AGENTID="应用ID"
 export TOUSER="@all或者账号"
 export MEDIA_ID="图片ID"
-export LOG_DIR='$log/log.txt'    #不用改
+export LOG_DIR="${log}/log.txt"    #不用改
 
 
 cd $node
 ./hlink restore $cachebackup/.hlink                                                    #还原缓存备份文件-如果重启不丢失缓存，请注释
-./hlink -c $config /mnt/disk2/Downloads/Temp /mnt/disk2/hlink2 > $log/log.txt          #自行修改硬链接路径
+./hlink -c $config /mnt/disk2/Downloads/ /mnt/disk2/hlink2/ > $log/log.txt             #自行修改硬链接路径
 bash $push/push.sh
-./hlink -c $config /mnt/disk3/Media/TV /mnt/disk3/hlink3/tv > $log/log.txt             #自行修改硬链接路径
-bash $push/push.sh
-./hlink -c $config /mnt/disk3/Media/Movie /mnt/disk3/hlink3/movie > $log/log.txt       #自行修改硬链接路径
-bash $push/push.sh
-./hlink -c $config /mnt/disk3/Media/Hiccup/1 /mnt/disk3/hlink3/1 > $log/log.txt    #自行修改硬链接路径
-bash $push/push.sh
-./hlink -c $config /mnt/disk3/Media/Hiccup/2 /mnt/disk3/hlink3/2 > $log/log.txt    #自行修改硬链接路径
+./hlink -c $config /mnt/disk3/Media/ /mnt/disk3/hlink3/ > $log/log.txt                 #自行修改硬链接路径(如果路径不够，复制粘贴即可)
 bash $push/push.sh
 ./hlink backup $cachebackup                                                            #备份缓存文件-如果重启不丢失缓存，请注释
